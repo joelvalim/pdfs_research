6420b30b1d27c6de0c5825c861e7c25b70f942c2
F5096724 Joel Valim <joevalim@bb.com.br>
2023-06-04 15:45:38 -0300

    novos pdfs

---
+++ create.bat
@@ -0,0 +1,7 @@
+echo "# pdfs_research" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/joelvalim/pdfs_research.git
+git push -u origin main
